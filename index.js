d20fbd9308822aa6d71f75b529e46742cfe00c21
Puspita Baidya <ug2102047@cse.pstu.ac.bd>
2026-06-12 23:37:05 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
-
+ 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
